d1c2052b2b42d2437bdace9aff3ce558ecb7eae8
katot213 <katot213@kklab.spub.chitose.ac.jp>
2018-04-08 13:32:38 +0900

    Tomcatと連携した際の変更

--- src/main/java/calendar/CalendarPage.java
+++ src/main/java/calendar/CalendarPage.java
@@ -2,9 +2,6 @@ package calendar;
 
 import org.apache.wicket.markup.html.WebPage;
 
-import com.googlecode.wicket.jquery.ui.Options;
-import com.googlecode.wicket.jquery.ui.calendar.Calendar;
-
 /**
  *
  * @author tkato
@@ -16,17 +13,17 @@ public class CalendarPage extends WebPage {
 
 	public CalendarPage() {
 
-		String message = getRequest().getPostParameters().getParameterValue("message").toString();
-
-		System.out.println(message);
-
-		Options options = new Options();
-		options.set("theme", true);
-		options.set("header", "{ left: 'title', right: 'month,agendaWeek,agendaDay, today, prev,next' }");
-
-		Calendar calendar = new Calendar("calendar", options);
-
-		this.add(calendar);
+//		String message = getRequest().getPostParameters().getParameterValue("message").toString();
+//
+//		System.out.println(message);
+//
+//		Options options = new Options();
+//		options.set("theme", true);
+//		options.set("header", "{ left: 'title', right: 'month,agendaWeek,agendaDay, today, prev,next' }");
+//
+//		Calendar calendar = new Calendar("calendar", options);
+//
+//		this.add(calendar);
 
 
 	}
